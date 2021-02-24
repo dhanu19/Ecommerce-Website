3eb3248b806b677a43c19a5cea71266d2b728ca6
dhanu19 <19dhanu99@gmail.com>
2021-02-24 15:49:01 +0530

    initial commit - 24/02 - entity input screens

---
+++ application/views/pages/cart.php
@@ -0,0 +1,38 @@
+
+<section class="cartItems">
+    
+    
+
+
+    <div class="container ">
+        <div class="container cartItemsHeader">
+            <h4>My Cart</h4>
+        </div>
+        
+    </div>
+
+
+
+
+
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ application/views/pages/home.php
@@ -0,0 +1,208 @@
+<!-- CAROUSAL -->
+<section id="1">
+    <div id="carousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active img-fluid ">
+            <img src="https://i.pinimg.com/originals/02/cf/cf/02cfcffac595c832c514d58704cd82ce.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+            </div>
+            </div>
+            <div class="carousel-item img-fluid">
+            <img src="https://i.pinimg.com/originals/ce/7d/24/ce7d24ecbd3eb25221795be24a3ac7bb.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+            </div>
+            </div>
+            <div class="carousel-item img-fluid">
+            <img src="http://www.theparadeswindon.co.uk/wp-content/uploads/2018/09/BW4579-The-Parade-Autumn-Winter-Web-Banner-1140x527px.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+            </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carousel"  data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carousel"  data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</section>
+
+<!-- CAROUSAL End -->
+
+<!-- PRODUCT DISPLAY BANNER -->
+
+<section class="ProductDisplayHeading">
+    <div class="container align-center">
+        <h4>Be a FASHIONISTA by wearing our range of products..</h4>
+    </div>
+</section>
+
+
+
+<!-- PRODUCT DISPLAY PAGE -->
+
+<section class="ProductDisplaySection prodDispstyle">
+    <div class="container">
+        <!-- grid row -->
+        <div class="row">
+            <!-- grid column -->
+            <div class="col-md-3 col-sm-6">
+                <!--card-->
+                <div class="card">
+                    <img src="https://images.bewakoof.com/t540/steel-blue-distressed-mid-rise-stretchable-women-s-jeans-women-s-stylized-denims-273155-1606201748.jpg" class="card-img-top" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Steel Blue Women's Jeans</h5>
+                        <p class="card-text">
+                        SKINNY FIT<br>Skinny Fit and Stretchable, designed to meet your fashion needs
+                        </p>
+                        <p>₹1049</p>
+                        <a href="#!" class="btn btn-primary">Add to cart</a>
+                    </div>
+                </div>
+                <!--card end -->
+            </div>
+
+            <!-- grid column -->
+            <div class="col-md-3 col-sm-6">
+               <!--card-->
+               <div class="card ">
+                    <img src="https://images.bewakoof.com/t540/ribbon-blue-mid-rise-stretchable-women-s-jeans-women-s-plain-denims-273121-1606201617.jpg" class="card-img-top" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Ribbon Blue Women's Jeans</h5>
+                        <p class="card-text">
+                        SKINNY FIT<br>Skinny Fit and Stretchable, designed to meet your fashion needs
+                        </p>
+                        <p>₹899</p>
+                        <a href="#!" class="btn btn-primary">Add to cart</a>
+                    </div>
+                </div>
+                <!--card end -->                
+            </div>
+            
+            <!-- grid column -->
+            <div class="col-md-3 col-sm-6">
+               <!--card-->
+               <div class="card">
+                    <img src="https://images.bewakoof.com/t540/backpack-blue-mid-rise-stretchable-women-s-jeans-women-s-plain-denims-273149-1606202520.jpg" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Backpack Blue Women's Jeans</h5>
+                        <p class="card-text">
+                        SKINNY FIT<br>Skinny Fit and Stretchable, designed to meet your fashion needs
+                        </p>
+                        <p>₹899</p>
+                        <a href="#!" class="btn btn-primary">Add to cart</a>
+                    </div>
+                </div>
+                <!--card end -->
+            </div>
+            
+            <!-- grid column -->
+            <div class="col-md-3 col-sm-6">
+               <!--card-->
+               <div class="card">
+                    <img src="https://images.bewakoof.com/t540/jade-black-distressed-mid-rise-stretchable-women-s-jeans-women-s-stylized-denims-273154-1606201506.jpg" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Jade Black Women's Jeans</h5>
+                        <p class="card-text">
+                        SKINNY FIT<br>Skinny Fit and Stretchable, designed to meet your fashion needs
+                        </p>
+                        <p>₹1049</p>
+                        <a href="#!" class="btn btn-primary">Add to cart</a>
+                    </div>
+                </div>
+                <!--card end -->
+            </div>
+        </div>
+        <!-- grid row end-->
+
+
+        <!-- grid row -->
+        <div class="row">
+            <!-- grid column -->
+            <div class="col-md-3 col-sm-6">
+                <!--card-->
+                <div class="card">
+                    <img src="https://images.bewakoof.com/t540/momo-pawri-boyfriend-t-shirts-bold-red-women-s-printed-boyfriend-t-shirts-333706-1613553242.jpg" class="card-img-top" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Momo Pawri T-Shirts</h5>
+                        <p class="card-text">
+                        OVERSIZED FIT<br>Falls loosely on the body.
+                        </p>
+                        <p>₹299</p>
+                        <a href="#!" class="btn btn-primary">Add to cart</a>
+                    </div>
+                </div>
+                <!--card end -->
+            </div>
+
+            <!-- grid column -->
+            <div class="col-md-3 col-sm-6">
+               <!--card-->
+               <div class="card ">
+                    <img src="https://images.bewakoof.com/t540/music-bear-half-sleeve-printed-t-shirt-white-women-s-half-sleeve-printed-t-shirt-329744-1612775816.jpg" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Music Bear T-Shirt </h5>
+                        <p class="card-text">
+                        REGULAR FIT<br>Fits just right - not too tight, not too loose.
+                        </p>
+                        <p>₹349</p>
+                        <a href="#!" class="btn btn-primary">Add to cart</a>
+                    </div>
+                </div>
+                <!--card end -->                
+            </div>
+            
+            <!-- grid column -->
+            <div class="col-md-3 col-sm-6">
+               <!--card-->
+               <div class="card">
+                    <img src="https://images.bewakoof.com/t540/bewakoof-smile-2-0-half-sleeve-printed-hoodie-t-shirt-women-s-half-sleeve-printed-hoodie-t-shirt-302894-1608816020.jpg" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Printed Hoodie T-Shirt</h5>
+                        <p class="card-text">
+                        REGULAR FIT<br>Fits just right - not too tight, not too loose.
+                        </p>
+                        <p>₹329</p>
+                        <a href="#!" class="btn btn-primary">Add to cart</a>
+                    </div>
+                </div>
+                <!--card end -->
+            </div>
+            
+            <!-- grid column -->
+            <div class="col-md-3 col-sm-6">
+               <!--card-->
+               <div class="card">
+                    <img src="https://images.bewakoof.com/t540/bewakoof-smile-2-0-round-neck-3-4-sleeve-t-shirts-women-s-printed-round-neck-3-4-sleeve-t-shirts-302915-1607931022.jpg" class="card-img-top" alt="..."/>
+                    <div class="card-body">
+                        <h5 class="card-title">Round Neck T-Shirts</h5>
+                        <p class="card-text">
+                        REGULAR FIT <br>Fits just right - not too tight, not too loose.
+                        </p>
+                        <p>₹329</p>
+                        <a href="#!" class="btn btn-primary">Add to cart</a>
+                    </div>
+                </div>
+                <!--card end -->
+            </div>
+            
+        </div>
+        <!-- grid row end-->
+    </div>
+
+</section>
+
+
